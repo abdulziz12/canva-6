2595b65336cb394b0062a6bbab96b7dcfe2ee6dd
abdulziz12 <abdurashidov200930@gmail.com>
2024-11-22 18:45:03 +0500

    nijokijuhj
    hbjki

---
+++ script.js
@@ -0,0 +1,10 @@
+var ikki = +prompt("ismingizni kiriting")
+var bir = +prompt("familiyangizni kiriting")
+var ikki = +prompt("yoshingizni kiriting")
+var bir = +prompt("yoqtirgan faningizni kiriting")
+var ikki = +prompt("yashaydigan tumaningizni kiriting")
+
+
+
+
+alert("malumotlaringizni kiritganingiz uchun rahmat")
